89a1268000938e4f19bbc41536270fb90e2d9348
hanseve <tezhans@gmail.com>
2020-01-08 14:31:59 +0800

    localhost Https/Wss support

--- README.md
+++ README.md
@@ -3,15 +3,29 @@
 A simple chat demo for socket.io
 
 ## How to use
+Tips: Only for local development not production!!! 
 
+```
+$ cd path/to/socket-chat-example
+$ mkdir ./encryption && cd ./encryption
+
+#create Self-signed certificate
+
+$ openssl genrsa -des3 -out server.pass.key 2048
+$ openssl rsa -in server.pass.key -out server.key
+ 
+$ openssl req -new -key server.key -out server.csr -subj "/C=CN/ST=Guangdong/L=ShenZhen/O=localhost/OU=localhost/CN=localshot.test"
+
+$ openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+```
+Then add server.crt file to you local keychain.(you can simply double click this file and follow the system tip)
 ```
 $ npm install
 $ npm run build
 $ npm run start
 ```
-
-And point your browser to `http://localhost:3000`. Optionally, specify
-a port by supplying the `PORT` env variable.
+You may need add ```127.0.0.1  localhost.test``` to your hosts.
+And point your browser to `http://localhost.test:4433`. 
 
 ## Features
 
@@ -20,6 +34,8 @@ on website load.
 - Users can type chat messages to the chat room.
 - A notification is sent to all users when a user joins or leaves
 the chatroom.
+- Secure connection with Https/Wss
+- Just for local development
 
 > Reference
 [https://github.com/socketio/socket.io/tree/master/examples/chat](https://github.com/socketio/socket.io/tree/master/examples/chat)
--- index.js
+++ index.js
@@ -2,22 +2,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+//http
+var http = require('http');
+var server = http.createServer(app);
 var port = process.env.PORT || 3000;
-
+var io = require('socket.io')(server);
 server.listen(port, () => {
-    console.log('Server listening at port %d', port);
+    console.log('Http Server listening at port %d', port);
 });
-
-// Routing
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Chatroom
 
 var numUsers = 0;
 var connectedUsername = {};
-
 io.on('connection', (socket) => {
     var addedUser = false;
 
@@ -36,7 +31,7 @@ io.on('connection', (socket) => {
         // whether nickname duplication or not
         if (connectedUsername[username]) {
             // tell client itself duplication
-            socket.emit('user:exists',{
+            socket.emit('user:exists', {
                 username: username,
             });
             return;
@@ -87,3 +82,92 @@ io.on('connection', (socket) => {
         }
     });
 });
+
+//https
+var fs = require('fs');
+var https = require('https');
+var options = {
+    key: fs.readFileSync('./encryption/server.key'),
+    cert: fs.readFileSync('./encryption/server.crt'),
+};
+var httpsServer = https.createServer(options, app);
+var ios = require('socket.io')(httpsServer);
+httpsServer.listen(4433, () => {
+    console.log('Https Server listening at port %d', 4433);
+});
+
+var httpsNumUsers = 0;
+var httpsConnectedUsername = {};
+ios.on('connection', (socket) => {
+    var addedUser = false;
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new:message', (data) => {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new:message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add:user', (username) => {
+        if (addedUser) return;
+        // whether nickname duplication or not
+        if (httpsConnectedUsername[username]) {
+            // tell client itself duplication
+            socket.emit('user:exists', {
+                username: username,
+            });
+            return;
+        }
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++httpsNumUsers;
+        addedUser = true;
+
+        httpsConnectedUsername[username] = 1;
+
+        socket.emit('login', {
+            numUsers: httpsNumUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user:joined', {
+            username: socket.username,
+            numUsers: httpsNumUsers
+        });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop:typing', () => {
+        socket.broadcast.emit('stop:typing', {
+            username: socket.username
+        });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            --httpsNumUsers;
+            delete httpsConnectedUsername[socket.username];
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user:left', {
+                username: socket.username,
+                numUsers: httpsNumUsers
+            });
+        }
+    });
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
